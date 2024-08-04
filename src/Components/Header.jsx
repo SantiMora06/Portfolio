b1a4d0a3a445b5ad4f8a52f6917c6259c1ba4fcb
Santi Mera <santigomero@gmail.com>
2024-08-04 20:19:50 +0300

    HomePage almost done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,41 +1,20 @@
-import { AppShell, Burger } from "@mantine/core";
-import { useDisclosure } from '@mantine/hooks';
 import classes from "../styles/header.module.css"
 import { Link } from "react-router-dom";
-
+import santi from "../assets/santi.png"
 
 const Header = () => {
 
-    const [opened, { toggle }] = useDisclosure();
 
     return (<>
-        <AppShell
-            header={{ height: 60 }}
-            navbar={{
-                width: 300,
-                breakpoint: 'sm',
-                collapsed: { mobile: !opened },
-            }}
-            padding="md"
-        >
-            <AppShell.Header>
-                <Burger
-                    opened={opened}
-                    onClick={toggle}
-                    hiddenFrom="sm"
-                    size="sm"
-                />
-                <ul className={classes.header}>
-                    <li> <Link to="/" className={classes.Navlink}>Logo</Link></li>
-                    <li> <Link to="/" className={classes.Navlink}>Home</Link></li>
-                    <li> <Link to="/projects" className={classes.Navlink}>Projects</Link></li>
-                    <li> <Link to="/about" className={classes.Navlink}> About</Link> </li>
-                    <li> <Link to="/contact" className={classes.Navlink}>Contact</Link></li>
-                </ul>
-            </AppShell.Header>
 
-            <AppShell.Main className={classes.main}>Main</AppShell.Main>
-        </AppShell>
+        <ul className={classes.header}>
+            <li> <Link to="/" className={classes.Navlink}>Home</Link></li>
+            <li> <Link to="/projects" className={classes.Navlink}>Projects</Link></li>
+            <li> <Link to="/about" className={classes.Navlink}> About</Link> </li>
+            <li> <Link to="/contact" className={classes.Navlink}>Contact</Link></li>
+            <li><img src={santi} alt="Santi Mora" /></li>
+        </ul>
+
     </>);
 }
 
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,33 @@
-import Header from "../Components/Header";
+import queseria from "../assets/queseria.png"
+import spear from "../assets/spear.png"
+import fatfuckers from "../assets/fatfuckers.png"
+import vintage from "../assets/Vintage.png"
 
 const HomePage = () => {
     return (<>
-        <Header />
+
+
+        <section className="container">
+            <section className="photo">
+                <section className="description">
+                    <h1>Hello, this is Santi Mora!</h1>
+                    <h2>I build websites</h2>
+                    <p className="content">Before starting, I want to thank you for checking my portfolio and I invite you to navigate all over the web to see what I am capable of and my current, but not static,
+                        skillset. You may think that I am just another new Web Developer, and in a sense I am, but thanks to my background as a photographer, journalist, content creator and
+                        Social Media Manager, I am far more than a simple applicant.</p>
+                </section>
+            </section>
+            <h3>Featured Projects</h3>
+            <section className="projectsPreview">
+                <ul>
+                    <li><div className="projectsItem"><img src={vintage} /> <div className="innerItem"> <span className="innertitle">Vintage Reborn</span></div> </div></li>
+                    <li><div className="projectsItem"><img src={spear} /><div className="innerItem"> <span className="innertitle">Spear The Fish!</span></div></div></li>
+                    <li><div className="projectsItem"><img src={queseria} /><div className="innerItem"> <span className="innertitle"> Queseria el Bregador</span></div></div></li>
+                    <li><div className="projectsItem"><img src={fatfuckers} /><div className="innerItem"> <span className="innertitle">FatFuckers</span></div></div></li>
+                </ul>
+            </section>
+
+        </section>
         HEADER
         HOME - PROJECTS - ABOUT - CONTACT
         MAIN
