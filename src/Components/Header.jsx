45ff8b6687368871f7153fb8375dc2a03c6d91ce
Santi Mera <santigomero@gmail.com>
2024-08-06 16:39:48 +0300

    Link for the CV Added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import classes from "../styles/header.module.css"
 import { Link } from "react-router-dom";
 import santi from "../assets/santi.png"
+import CV from "../assets/NEWCVSANTI.pdf"
 
 const Header = () => {
 
@@ -12,6 +13,13 @@ const Header = () => {
                 <li className={classes.navItem}> <Link to="/projects" className={classes.Navlink}>Projects</Link></li>
                 <li className={classes.navItem}> <Link to="/about" className={classes.Navlink}> About</Link> </li>
                 <li className={classes.navItem}><img src={santi} alt="Santi Mora" /></li>
+                <li className={classes.Navlink}> <Link
+                    to={CV}
+                    download="Santi's CV"
+                    target="_blank"
+                    rel="noreferrer"
+                >Download CV</Link></li>
+
             </ul>
         </section>
     </>);
